bb1cea869c52411702991e68251d42eb4f5a285f
seanzhangJM <121948853@qq.com>
2022-01-05 09:42:26 +0800

    pytorch 训练模板 v2 修改GPU和CPU兼容

--- util/root_config.py
+++ util/root_config.py
@@ -17,6 +17,7 @@ DATA_DIR = NAS_DIR / 'data'
 MODEL_DIR = NAS_DIR / 'model_save'
 PICTURE_DIR = NAS_DIR / 'picture'
 LOG_DIR = NAS_DIR / 'log'
+TRAIN_DATA_DIR = BASE_DIR / 'data'
 
 # ************************ 日志级别 ************************
 LOG_LEVEL = 'DEBUG'
--- util/train_and_test.py
+++ util/train_and_test.py
@@ -8,6 +8,7 @@
 
 import torch
 from torch import nn
+from d2l import torch as d2l
 from torch.utils import data
 from pytorch_default_demo.util.bean import Accumulator, Animator, Timer
 from pytorch_default_demo.util.log_config import get_logger
@@ -106,3 +107,48 @@ def train(net, train_iter, test_iter, num_epochs, lr, device):
           f'test acc {test_acc:.3f}')
     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
           f'on {str(device)}')
+
+
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+
+def k_fold(net, k, X_train, y_train, num_epochs, learning_rate, weight_decay, batch_size):
+    """
+    修改后，可以用于传统模型，深度学习中基本不用这个
+    :param net:
+    :param k:
+    :param X_train:
+    :param y_train:
+    :param num_epochs:
+    :param learning_rate:
+    :param weight_decay: 正则衰减
+    :param batch_size:
+    :return:
+    """
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate, weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.plot(list(range(1, num_epochs + 1)), [train_ls, valid_ls],
+                     xlabel='epoch', ylabel='rmse', xlim=[1, num_epochs],
+                     legend=['train', 'valid'], yscale='log')
+        print(f'折{i + 1}，训练log rmse{float(train_ls[-1]):f}, '
+              f'验证log rmse{float(valid_ls[-1]):f}')
+    return train_l_sum / k, valid_l_sum / k
